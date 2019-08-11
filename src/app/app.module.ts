2350bdfd5ac6e221fe8f51ff6211513e92f8dd50
Lordson Jacob <lordsonpaulraj@gmail.com>
2019-08-11 12:01:52 +0530

    initialized routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
-import { HomeImageComponent } from './home-image/home-image.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent,
-    HomeImageComponent
+    routingComponents,
   ],
   imports: [
     BrowserModule,
