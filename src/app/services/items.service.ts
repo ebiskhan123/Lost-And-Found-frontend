c8411237335c1f3d51988f9a1d7191b19f5a964b
Ebenezer Ajay Williams <ebby.williams1996@gmail.com>
2019-08-16 14:55:53 +0530

    Item Service

--- src/app/services/items.service.ts
+++ src/app/services/items.service.ts
@@ -1,9 +1,66 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import User from './models/user.model';
+import 'rxjs/Rx';
+import Observable from 'rxjs/Observable';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemsService {
+  private const serverurl: string = 'url'
 
-  constructor() { }
+  constructor(private http: Htttp) { }
+
+  getItems(location: string, category: string, lostorfound: string){
+    let url:string = serverurl;
+    if(location!=null){
+      url +='?location='+location;
+    }
+    if(category!=null){
+      if(url.indexOf('?') > -1){
+        url += '&category='+category;
+      }
+      else{
+        url +='?category='+category;
+      }
+    }
+    if(lostorfound!=null){
+      if(url.indexOf('?') > -1){
+        url += '&lostorfound='+lostorfound;
+      }
+      else{
+        url +='?lostorfound='+lostorfound;
+      }
+    }
+    return getItemsAjax(url);
+  }
+
+  getItemsAjax(url:string) {
+    return this.http.get(url, user).map(
+      (response: Response) => {
+        const data = response.json();
+        return data;
+      }
+    ).catch(
+      (error: Response) => {
+        return Observable.throw("Error occured");
+      }
+    );
+  }
+
+
+  addItems(item: any[]) {
+    return this.http.post(loginurl, item).map(
+      (response: Response) => {
+        const data = response.json();
+        return data;
+      }
+    ).catch(
+      (error: Response) => {
+        return Observable.throw("Error occured");
+      }
+    );
+  }
 }
